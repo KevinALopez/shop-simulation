dbf3bfd8594f59d5419d3ffa61a72bbaa5ce0183
KevinALopez <005.kevin.ayala@gmail.com>
2025-01-21 21:40:56 +0100

    implement addPlantToCart function

--- js/event.js
+++ js/event.js
@@ -5,21 +5,62 @@ const shoppingCartIcon = document.querySelector("#cart-icon");
 let isCartOpen = false;
 
 /**
+ * PLANT CRUD
+ */
+function getPlantById(id) {
+    return plants.find((plant) => plant.id === id);
+}
+/**
+ * PLANT CRUD
+ */
+
+/**
  * CART FUNCTIONS
  */
-function showCart() {
-    if (isCartOpen) {
+function closeCart() {
+    if (
+        navigator.userAgent.includes("iPhone") ||
+        navigator.userAgent.includes("Android")
+    ) {
+        shoppingCart.style.bottom = "-15rem";
+    } else {
         shoppingCart.style.left = "-25rem";
-        isCartOpen = false;
+    }
+}
+
+function openCart() {
+    if (
+        navigator.userAgent.includes("iPhone") ||
+        navigator.userAgent.includes("Android")
+    ) {
+        shoppingCart.style.bottom = "0";
     } else {
         shoppingCart.style.left = "-0.5rem";
+    }
+}
+
+function toggleCart() {
+    console.log(navigator.userAgent);
+    if (isCartOpen) {
+        closeCart();
+        isCartOpen = false;
+    } else {
+        openCart();
         isCartOpen = true;
     }
 }
 
-function addPlantToCart(event) {}
+function addPlantToCart(event) {
+    const plantId = Number(event.target.dataset.plantId);
+    const plant = getPlantById(plantId);
+
+    cart.push(plant);
+    updateTotal();
+}
 
 function removePlantFromCart(event) {}
+
+function updateTotal() {}
 
 function emptyCart(event) {}
 
@@ -62,5 +103,5 @@ function printAllPlants(plantsArr, dom) {
  * PRINT FUNCTIONS
  */
 
-shoppingCartIcon.addEventListener("click", showCart);
+shoppingCartIcon.addEventListener("click", toggleCart);
 printAllPlants(plants, gridSection);
